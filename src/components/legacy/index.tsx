3a8ef6de1fd328dc6ea6505b630ede0102ff2126
mittt777 <121394585+mittt777@users.noreply.github.com>
2025-02-12 13:05:41 +0800

    Update index.tsx

--- src/components/legacy/index.tsx
+++ src/components/legacy/index.tsx
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
 import { Demo } from "./demo";
 import { Intro } from "./intro";
 import { Foot } from "./foot";
